954678a39c3decb898671aa01ac5cb9b22ae5270
blackeye75 <priyanshuraj22275@gmail.com>
2024-07-19 21:56:05 +0530

    nav

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./assets/components/navbar/Navbar";
+
+function App() {
+  return (
+    <main className="overflow-x-hidden">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
